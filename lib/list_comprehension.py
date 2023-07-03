056cf3ee8f40d52aa2f1fc7b77c92c0250fc24f3
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-07-03 14:21:46 -0700

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = []
+
+    for num in num_list:
+        if num % 2 == 0:
+            evens.append(num)
+
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed = []
+
+    for sentence in sentence_list:
+        exclaimed.append(f"{sentence}!")
+
+    return exclaimed
